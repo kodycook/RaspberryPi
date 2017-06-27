47b2cca9f70841d182da1746781777ec9d6fe7ec
kodycook <masterkcook@hotmail.com>
2017-06-27 17:20:46 +1000

    Cleaned Up Update of Earlier Test
    This is extension of commit "This is a Test Commit".
    
    I also:
    - Updated the gitignore file to exclude pycharm-related files

--- src/FroniusListener/PostClient.py
+++ src/FroniusListener/PostClient.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+import socket
 
 
 
@@ -44,20 +45,26 @@ def run():
     # Test
     # Server settings
     # Choose port 8080, for port 80, which is normally used for a http server, you need root access
-    server_address = ('10.1.1.15', 40)
-    httpd = HTTPServer(server_address, PostClient)
-    httpd.serve_forever()
+    # server_address = ('10.1.1.15', 40)
+    # httpd = HTTPServer(server_address, PostClient)
+    # httpd.serve_forever()
+
+    server = socket.socket()
+    server.bind(('10.1.1.15', 40))
+    server.listen(4)
+    client_socket, client_address = server.accept()
+    print(client_address, "has connected")
+    while 1 == 1:
+        recieved_data = client_socket.recv(1024)
+        ConvertJson(recieved_data)
 
 def ConvertJson(text):
-    #print(text)
+    print(text)
 
-    #soup = BeautifulSoup(text, 'lxml')
-    #packet = str(soup.html.p)
-
-    data = json.loads(text)
-    print("Timestamp: {0}".format(data["Head"]["Timestamp"]))
-    print("Current Production: {0}{1}".format(data["Body"]["PAC"]["Values"]["1"],data["Body"]["PAC"]["Unit"]))
-    print("Today's Yeild: {0}{1}".format(data["Body"]["DAY_ENERGY"]["Values"]["1"],data["Body"]["DAY_ENERGY"]["Unit"]))
+    # data = json.loads(text)
+    # print("Timestamp: {0}".format(data["Head"]["Timestamp"]))
+    # print("Current Production: {0}{1}".format(data["Body"]["PAC"]["Values"]["1"],data["Body"]["PAC"]["Unit"]))
+    # print("Today's Yeild: {0}{1}".format(data["Body"]["DAY_ENERGY"]["Values"]["1"],data["Body"]["DAY_ENERGY"]["Unit"]))
     #print(data[""])
     #print(data[""])
 
