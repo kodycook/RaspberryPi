0f6ac53f5c98c8ecd4868bfb96bb28b4173467ca
kodycook <masterkcook@hotmail.com>
2017-06-27 17:08:28 +1000

    Addtional Commit
    GitHub still showed files to be commited.

--- src/FroniusListener/PostClient.py
+++ src/FroniusListener/PostClient.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+import socket
 
 
 
@@ -40,31 +41,33 @@ class PostClient(BaseHTTPRequestHandler):
 def run():
     print('Starting Fronius Listener')
 
-
+    #server_address = ('10.1.1.15', 40)
     # Test
     # Server settings
     # Choose port 8080, for port 80, which is normally used for a http server, you need root access
-    server_address = ('10.1.1.15', 40)
-    httpd = HTTPServer(server_address, PostClient)
-    httpd.serve_forever()
+    # server_address = ('10.1.1.15', 40)
+    # httpd = HTTPServer(server_address, PostClient)
+    # httpd.serve_forever()
+
+    server = socket.socket()
+    server.bind(('10.1.1.15', 40))
+    server.listen(4)
+    client_socket, client_address = server.accept()
+    print(client_address, "has connected")
+
+    while 1 == 1:
+        recieved_data = client_socket.recv(1024)
+        print(recieved_data)
+        ConvertJson(recieved_data)
 
 def ConvertJson(text):
-    #print(text)
+    print("Here")
 
-    #soup = BeautifulSoup(text, 'lxml')
-    #packet = str(soup.html.p)
+    #data = json.loads(text)
+    #print("Timestamp: {0}".format(data["Head"]["Timestamp"]))
+    #print("Current Production: {0}{1}".format(data["Body"]["PAC"]["Values"]["1"],data["Body"]["PAC"]["Unit"]))
+    #print("Today's Yeild: {0}{1}".format(data["Body"]["DAY_ENERGY"]["Values"]["1"],data["Body"]["DAY_ENERGY"]["Unit"]))
 
-    data = json.loads(text)
-    print("Timestamp: {0}".format(data["Head"]["Timestamp"]))
-    print("Current Production: {0}{1}".format(data["Body"]["PAC"]["Values"]["1"],data["Body"]["PAC"]["Unit"]))
-    print("Today's Yeild: {0}{1}".format(data["Body"]["DAY_ENERGY"]["Values"]["1"],data["Body"]["DAY_ENERGY"]["Unit"]))
-    #print(data[""])
-    #print(data[""])
-
-
-
-    #print(type(obj))
-    #print(obj.tostring())
 
 
 run()
