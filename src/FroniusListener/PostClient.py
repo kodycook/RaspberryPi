586173bd7c3e160d706ceec1c8aaef24f0549bb6
kodycook <masterkcook@hotmail.com>
2017-07-01 14:42:43 +1000

    Added Old HTTP Implementation Into new project

--- src/FroniusListener/PostClient.py
+++ src/FroniusListener/PostClient.py
@@ -1,41 +1,47 @@
 #!/usr/bin/env python
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
-import socket
-
-# HTTPRequestHandler class
-from bs4 import BeautifulSoup
-
 
 class PostClient(BaseHTTPRequestHandler):
 
     def __init__(self):
         run()
 
+    def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+
+        # Send headers
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        # Send message back to client
+        message = "Hello world!"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
+
+    def do_POST(self):
+        def do_GET(self):
+            # Send response status code
+            self.send_response(200)
+
+            # Send headers
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+        print(int(self.headers['Content-Length']))
+        self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+
+
+
 def run():
     print('Starting Fronius Listener')
-
-    server = socket.socket()
-    server.bind(('10.1.1.15', 40))
-    server.listen(4)
-    client_socket, client_address = server.accept()
-    print(client_address, "has connected")
-    while 1 == 1:
-        rfile = client_socket.makefile('rb', -1)
-        data_string = rfile.read(
-            int(client_socket.http.client.parse_headers(rfile, _class=http.client.HTTPMessage)['Content-Length']))
-        recieved_data = client_socket.recv(1024)
-        soup = BeautifulSoup(recieved_data.decode("utf-8"), "lxml")
-        ConvertJson(soup.p)
-
-
+    server_address = ('10.1.1.15', 40)
+    httpd = HTTPServer(server_address, PostClient)
+    httpd.serve_forever()
 
 def ConvertJson(text):
     data = json.loads(text)
-
-
     print("Timestamp: {0}".format(data["Head"]["Timestamp"]))
     print("Current Production: {0}{1}".format(data["Body"]["PAC"]["Values"]["1"],data["Body"]["PAC"]["Unit"]))
     print("Today's Yeild: {0}{1}".format(data["Body"]["DAY_ENERGY"]["Values"]["1"],data["Body"]["DAY_ENERGY"]["Unit"]))
-    #print(data[""])
-    #print(data[""])
